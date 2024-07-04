938945e6b63f587aacde513d758ca2d27a95cd74
MonzurAlam <you@example.com>
2024-07-04 16:40:02 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+}
+
+// monzuralam16
+// aurora01234
